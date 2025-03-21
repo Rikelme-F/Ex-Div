59d3b9643b39ddd145092359a22f4d8aff924eeb
Rikelme-F <Rikelme.fsantos@senacsp.edu.br>
2025-03-20 21:21:08 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercício2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um número");
+            string numero1 = Console.ReadLine();
+
+            Console.WriteLine("Escreva mais um número");
+            string numero2 = Console.ReadLine();
+
+            double num1 = Convert.ToDouble(numero1);
+            double num2 = Convert.ToDouble(numero2);
+
+            double div = num1 / num2;
+
+            Console.WriteLine("A divisão dos números é:" + div);
+        }
+    }
+}
